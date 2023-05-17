3cbe43ebc7a863f58f559c254ab5568b518bb30c
AnshumanPatek <78948391+AnshumanPatek@users.noreply.github.com>
2023-05-17 17:14:50 +0530

    Create 1512. Number of Good Pairs.java

---
+++ 1512. Number of Good Pairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+    
+        int count =0;
+        int n = nums.length;
+        if(n==0){
+            return -1;
+        }
+        for(int i = 0; i<n;i++){
+            for(int j =i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
